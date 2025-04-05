70b34b47825e1bcc03449c610e8e1a2309c510f3
Christian Sorensen <ctsorensen2019@gmail.com>
2025-04-04 19:35:48 -0600

    Removed NavBar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,17 +2,14 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import NoPage404 from './pages/NoPage404';
-import Layout from './pages/Layout';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Navigate to="/home" replace />} />
-          <Route path="home" element={<Home />} />
-          <Route path="*" element={<NoPage404 />} />
-        </Route>
+        <Route index element={<Navigate to="/home" replace />} />
+        <Route path="home" element={<Home />} />
+        <Route path="*" element={<NoPage404 />} />
       </Routes>
     </BrowserRouter>
   );
