384c1f23b0a84ca650ee918715987bc7cc132e1a
John Gibson <johnmgibson3@gmail.com>
2025-04-07 13:23:58 -0600

    register page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import NoPage404 from './pages/NoPage404';
 import Layout from './pages/Layout';
 import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
@@ -14,8 +15,10 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Navigate to="/home" replace />} />
           <Route path="home" element={<Home />} />
-          <Route path="*" element={<NoPage404 />} />
           <Route path="login" element={<Login/>} />
+          <Route path="register" element={<Register/>} />
+          <Route path="*" element={<NoPage404 />} />
+          
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ frontend/src/pages/Register.tsx
@@ -0,0 +1,5 @@
+import RegisterForm from '../components/Register/RegisterForm';
+
+export default function Register() {
+  return <RegisterForm />;
+}
