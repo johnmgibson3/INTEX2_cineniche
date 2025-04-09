5dcd4d2671ab83733092f5b5ddabfbd461198496
benjiblas11 <153689062+benjiblas11@users.noreply.github.com>
2025-04-09 17:33:14 -0600

    Push before I pull from main

--- frontend/src/types/HybridRecommender.ts
+++ frontend/src/types/HybridRecommender.ts
@@ -3,17 +3,17 @@ export interface Recommend {
     originalMovieTitle?: string;
     rec1Id?: string;
     rec1Title?: string;
-    rec1Score?: string;
+    rec1Score?: number;
     rec2Id?: string;
     rec2Title?: string;
-    rec2Score?: string;
+    rec2Score?: number;
     rec3Id?: string;
     rec3Title?: string;
-    rec3Score?: string;
+    rec3Score?: number;
     rec4Id?: string;
     rec4Title?: string;
-    rec4Score?: string;
+    rec4Score?: number;
     rec5Id?: string;
     rec5Title?: string;
-    rec5Score?: string;
+    rec5Score?: number;
 }
