2d7d9780fcab21261e29fffb1853aa6d037a6cb2
John Gibson <163345756+johnmgibson3@users.noreply.github.com>
2025-05-30 00:14:14 -0600

    Update RatingsAPI.ts

--- frontend/src/api/RatingsAPI.ts
+++ frontend/src/api/RatingsAPI.ts
@@ -1,6 +1,6 @@
 import { Rating } from '../types/Rating';
 
-const API_URL = import.meta.env.VITE_API_URL || 'https://intex-backend7-c2cghsf3cbddhdfm.centralus-01.azurewebsites.net/api';
+const API_URL = import.meta.env.VITE_API_URL || 'https://cinenicheback-arfmceaefxg5dqb9.westcentralus-01.azurewebsites.net/api';
 
 export const getRatings = async (): Promise<Rating[] | null> => {
   try {
@@ -21,7 +21,7 @@ export async function submitRating(
   rating: number
 ): Promise<boolean> {
 
-  const res = await fetch('https://intex-backend7-c2cghsf3cbddhdfm.centralus-01.azurewebsites.net/api/Ratings/Add', {
+  const res = await fetch('https://cinenicheback-arfmceaefxg5dqb9.westcentralus-01.azurewebsites.net/api/Ratings/Add', {
 
     method: 'POST',
     headers: {
@@ -36,7 +36,7 @@ export async function submitRating(
 export async function getAverageRating(showId: string): Promise<number | null> {
   try {
     const response = await fetch(
-      `https://intex-backend7-c2cghsf3cbddhdfm.centralus-01.azurewebsites.net/api/Ratings/Average/${showId}`
+      `https://cinenicheback-arfmceaefxg5dqb9.westcentralus-01.azurewebsites.net/api/Ratings/Average/${showId}`
     );
     if (!response.ok) {
       throw new Error('Failed to fetch average rating');
